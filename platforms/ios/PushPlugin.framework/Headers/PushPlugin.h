404d9e42ddb2540473f4eb654784bf9d98274053
dnelson-simplec <dnelson@simplec.com>
2019-05-13 12:10:47 -0400

    include updates for strict ios

--- platforms/ios/PushPlugin.framework/Headers/PushPlugin.h
+++ platforms/ios/PushPlugin.framework/Headers/PushPlugin.h
@@ -8,5 +8,5 @@ FOUNDATION_EXPORT const unsigned char PushPluginVersionString[];
 
 // In this header, you should import all the public headers of your framework using statements like #import <PushPlugin/PublicHeader.h>
 
-#import <Push.h>
-#import <PushManager.h>
+#import <PushPlugin/Push.h>
+#import <PushPlugin/PushManager.h>
